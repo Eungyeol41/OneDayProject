ccf51b34511b30c81dae3a2804ca9d6ad4315a44
Eungyeol41 <iniziato@naver.com>
2021-06-19 18:17:28 +0900

    dao 수정

--- SpMVC_One_01_Score/SpMVC_One_01_Score/src/main/java/com/callor/score/persistance/ScoreDao.java
+++ SpMVC_One_01_Score/SpMVC_One_01_Score/src/main/java/com/callor/score/persistance/ScoreDao.java
@@ -6,7 +6,7 @@ import com.callor.score.model.ScoreVO;
 
 public interface ScoreDao extends GenericDao<ScoreVO, String>{
 
-	public List<ScoreVO> findByStNum(String st_num);
+	public List<ScoreVO> findByStNum(String sc_stnum);
 	public String findByMaxCode();
 	
 }
--- SpMVC_One_01_Score/SpMVC_One_01_Score/src/main/java/com/callor/score/persistance/impl/ScoreDaoImplV1.java
+++ SpMVC_One_01_Score/SpMVC_One_01_Score/src/main/java/com/callor/score/persistance/impl/ScoreDaoImplV1.java
@@ -2,25 +2,52 @@ package com.callor.score.persistance.impl;
 
 import java.util.List;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import com.callor.score.model.ScoreVO;
+import com.callor.score.model.StudentVO;
 import com.callor.score.persistance.ScoreDao;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository("scoreDaoV1")
 public class ScoreDaoImplV1 implements ScoreDao{
+	
+	protected JdbcTemplate jdbcTemplate;
+	public ScoreDaoImplV1(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@Override
 	public List<ScoreVO> selectAll() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		String sql = " SELECT * FROM tbl_score ";
+		
+		List<ScoreVO> scList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<ScoreVO>(ScoreVO.class));
+		log.debug("SELECT {}", scList.toString());
+		
+		return scList;
 	}
 
 	@Override
 	public ScoreVO findById(String pk) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		String sql = " SELECT * FROM tbl_score ";
+		sql += " WHERE sc_stnum = ? ";
+		
+		ScoreVO scVO = (ScoreVO) jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ScoreVO.class));
+		log.debug("FINDBYID {}", scVO.toString()); 
+		
+		return scVO;
 	}
 	
 	@Override
-	public List<ScoreVO> findByStNum(String st_num) {
+	public List<ScoreVO> findByStNum(String sc_stnum) {
 		// TODO Auto-generated method stub
 		return null;
 	}
